499ea48894b120b75996312eba33bc4117df6df1
anji0114 <anji.tnk.140114@gmail.com>
2022-09-29 13:55:39 +0900

    fadeIn アニメーションの追加

--- src/js/animation.js
+++ src/js/animation.js
@@ -6,8 +6,6 @@ gsap.registerPlugin(ScrollTrigger);
 export const animation = () => {
   gsap.to("#area01 .col", {
     rotation: 360, // 180度回転する
-    duration: 5, // 3秒間アニメーションする
-    opacity: 1,
     scrollTrigger: {
       trigger: "#area01",
       start: "top center",
@@ -59,4 +57,58 @@ export const animation = () => {
       },
     }
   );
+
+  gsap.fromTo(
+    "#area05 .col",
+    {
+      y: -200, // x軸方向に500px移動する
+      opacity: 0,
+    },
+    {
+      y: 0,
+      duration: 3, // 3秒間アニメーションする
+      opacity: 1,
+      scrollTrigger: {
+        trigger: "#area03",
+        start: "top center",
+        end: "bottom center",
+        markers: true,
+      },
+    }
+  );
+
+  gsap.to("#area04 .col", {
+    x: 300,
+    scrollTrigger: {
+      trigger: "#area04",
+      start: "top center",
+      end: "bottom center",
+      scrub: true,
+      markers: true,
+    },
+  });
+
+  const els = document.querySelectorAll(".fadeIn-area .col");
+
+  els.forEach((el, index) => {
+    console.log(index);
+    gsap.fromTo(
+      el,
+      {
+        y: 100, // x軸方向に100px移動する
+        opacity: 0,
+      },
+      {
+        y: 0,
+        duration: 2, // 2秒間アニメーションする
+        opacity: 1,
+        scrollTrigger: {
+          trigger: el.closest(".fadeIn-area"), //親要素指定
+          start: "top center",
+          end: "bottom center",
+          markers: true,
+        },
+      },
+    );
+  });
 };
